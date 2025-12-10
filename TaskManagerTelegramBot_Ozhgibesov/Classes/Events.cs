e3418e87f6f394752f7cf2e4c323a34bc2b2cb43
aaa <aaaa@com>
2025-12-10 14:02:32 +0500

    Созданные классы для приложения. Ожгибесов.

---
+++ TaskManagerTelegramBot_Ozhgibesov/Classes/Events.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagerTelegramBot_Ozhgibesov.Classes
+{
+    public class Events
+    {
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+        public Events(DateTime time, string message)
+        {
+            Time = time;
+            Message = message;
+        }
+    }
+}
---
+++ TaskManagerTelegramBot_Ozhgibesov/Classes/Users.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagerTelegramBot_Ozhgibesov.Classes
+{
+    public class Users
+    {
+        public long IdUser { get; set; }
+        public List<Events> Events { get; set; }
+        public Users(long idUser)
+        {
+            IdUser = idUser;
+            Events = new List<Events>();
+        }
+    }
+}
