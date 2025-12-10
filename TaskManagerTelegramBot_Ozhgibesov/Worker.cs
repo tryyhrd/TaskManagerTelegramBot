f1304c998dc6a8036e8d6e7b1843d17f7b54825a
aaa <aaaa@com>
2025-12-10 14:25:33 +0500

    Разработанные методы отправки сообщений и команд. Ожгибесов

--- TaskManagerTelegramBot_Ozhgibesov/Worker.cs
+++ TaskManagerTelegramBot_Ozhgibesov/Worker.cs
@@ -1,5 +1,6 @@
 using TaskManagerTelegramBot_Ozhgibesov.Classes;
 using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TaskManagerTelegramBot_Ozhgibesov
@@ -65,6 +66,48 @@ namespace TaskManagerTelegramBot_Ozhgibesov
             inlineKeyboards.Add(new InlineKeyboardButton("Удалить", Message));
 
             return new InlineKeyboardMarkup(inlineKeyboards);
+        }
+
+        public async void SendMessage(long chatId, int typeMessage)
+        {
+            if (typeMessage != 3)
+            {
+                await telegramBotClient.SendMessage(
+                    chatId,
+                    Messages[typeMessage],
+                    ParseMode.Html,
+                    replyMarkup: GetButtons());
+            }
+            else if (typeMessage == 3)
+                await telegramBotClient.SendMessage(
+                    chatId,
+                    $"Указанное вами время и дата не могут быть установлены," +
+                    $"потому-что сейчас уже: {DateTime.Now.ToString("HH.mm dd.MM.yyyy")}");
+        }
+
+        public async void Command(long chatId, string command)
+        {
+            if (command.ToLower() == "/start") SendMessage(chatId, 0);
+            else if (command.ToLower() == "/create_task") SendMessage(chatId, 1);
+            else if (command.ToLower() == "/list_tasks")
+            {
+                Users User = Users.Find(x => x.IdUser == chatId);
+
+                if (User == null) SendMessage(chatId, 4);
+                else if (User.Events.Count == 0) SendMessage(chatId, 4);
+                else
+                {
+                    foreach (Events Event in User.Events)
+                    {
+                        await telegramBotClient.SendMessage(
+                            chatId,
+                            $"Уведомить пользователя: {Event.Time.ToString("HH:mm dd:MM:yyyy")}" +
+                            $"\nСообщение: {Event.Message}",
+                            replyMarkup: DeleteEvent(Event.Message)
+                            ); 
+                    }
+                }
+            }
         }
     }
 }
