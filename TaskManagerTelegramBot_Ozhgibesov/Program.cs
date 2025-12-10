ba7c3e1879acf184d8b123041dc66238ca7f1db4
aaa <aaaa@com>
2025-12-10 13:57:27 +0500

    Добавьте файлы проекта.

---
+++ TaskManagerTelegramBot_Ozhgibesov/Program.cs
@@ -0,0 +1,10 @@
+using TaskManagerTelegramBot_Ozhgibesov;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
